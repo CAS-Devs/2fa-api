8fab3da09a7fee0c80b647b461add9155f59a22d
Lucas Cascao <lucas.rama@castecnologia.com.br>
2020-12-29 11:49:31 -0300

    issue_3 Foram inseridos classes DTOs para responder as requisições

---
+++ src/main/kotlin/br/com/twofa/server/dto/LoginDTO.kt
@@ -0,0 +1,5 @@
+package br.com.twofa.server.dto
+
+data class LoginDTO(
+    val isLoginAuthorized: Boolean
+)
---
+++ src/main/kotlin/br/com/twofa/server/dto/TokenDTO.kt
@@ -0,0 +1,5 @@
+package br.com.twofa.server.dto
+
+data class TokenDTO(
+    val isValidated: Boolean
+)
